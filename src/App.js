9207ae8f47ef2ddf4dc0d61c092e30c590e2efe4
Dluna.dev <69652080+Dlunadev7@users.noreply.github.com>
2021-11-15 23:56:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { FabWhatsapp } from './components/fabs/whatsapp/FabWhatsapp';
+import React from 'react'
+import { FabWhatsapp } from './components/fabs/whatsapp/FabWhatsapp'
 import { Footer } from './components/footer/Footer';
 import { Header } from './components/header/Header';
 import { Main } from './components/main/Main';
 import { Navbar } from './components/navbar/Navbar';
 import { FirstSection } from './components/sections/FirstSection';
-import { FiveSection } from './components/sections/FiveSection';
+import { FiveSection } from './components/sections/FiveSection'
 import { FourSection } from './components/sections/FourSection';
 import { SecondSection } from './components/sections/SecondSection';
 import { SixSection } from './components/sections/SixSection';
